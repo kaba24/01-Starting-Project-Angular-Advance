7ced73ca71e235547a4bdd73ff35185fed82d9af
Vakaba Doumbia <Vakisbke2@gmail.com>
2024-07-24 17:55:07 -0400

    changes

---
+++ Downloads/angularProject/01-starting-project-advance-angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './server-status/server-status.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports:[HeaderComponent,ServerStatusComponent, TrafficComponent,SupportTicketsComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+ 
+ 
+}
